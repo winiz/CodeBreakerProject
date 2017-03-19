f75536dd08e1d57eef84fca0b0a87a457e097313
xinranzhang <xza116@sfu.ca>
2017-03-19 18:33:56 +0800

    123

--- assets/main.js
+++ assets/main.js
@@ -40,10 +40,10 @@ function validateInput(guess){
 function getResults(input){
   let temp = <div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">;
   for (i=0;i<input.length;i++){
-    if(input.charAt(i)==answer.value.charAt(i)){
+    if(input.charAt(i) == answer.value.charAt(i)){
       temp+=`<span class="glyphicon glyphicon-ok"></span>`;
     }
-    else if(anwer.value.indexOf(input.charAt(i)) > -1){
+    else if(answer.value.indexOf(input.charAt(i)) > -1){
       temp+=`<span class="glyphicon glyphicon-transfer"></span>`;
     }
     else
